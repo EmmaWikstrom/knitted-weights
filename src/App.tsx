4da8b4bf93cbc1648d75dba84f0f1f62ca1f2eeb
siritell <siri.tell@hotmail.com>
2026-04-20 14:54:58 +0200

    Added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import { Button } from "./Components/Button/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>Knitted Weights</h1>
-      <p>
-        Welcome to Knitted Weights! This is a simple app to help you keep track
-        of your knitting projects and the weights of the yarn you use.
-      </p>
+
+      <Button
+        variant="tertiary"
+        label="Add task"
+        onClick={() => console.log("Task added")}
+      />
+      <Button
+        variant="primary"
+        label="Edit"
+        onClick={() => console.log("Task edited")}
+      />
+      <Button
+        variant="secondary"
+        label="Delete"
+        onClick={() => console.log("Task deleted")}
+      />
     </div>
   );
 }
